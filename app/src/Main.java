75a331e47c0448ecc327019cf4374e04e7db1156
ewa.miazga <ewa.miazga@redge.com>
2022-10-20 17:33:49 +0200

    add documentation files

--- app/src/Main.java
+++ app/src/Main.java
@@ -1,4 +1,14 @@
+/**
+ * Class Main
+ *
+ * @author ewa.miazga
+ */
 public class Main {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
