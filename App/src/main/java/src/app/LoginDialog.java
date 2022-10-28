0348764e1fb1e425836dc3d1ca3f93c05fb648d7
ewa.miazga <ewa.miazga@redge.com>
2022-10-28 17:56:30 +0200

    add atributes to class LoginDialog

--- App/src/main/java/src/app/LoginDialog.java
+++ App/src/main/java/src/app/LoginDialog.java
@@ -3,6 +3,13 @@ package src.app;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -11,7 +18,14 @@ import javafx.stage.Stage;
  * @author ewa.miazga
  */
 public class LoginDialog extends Application implements EventHandler<ActionEvent> {
-
+    private GridPane grid;
+    private Text formTitle, notification;
+    private Label userLoginLabel, userPassLabel;
+    private TextField userLoginField;
+    private PasswordField userPassField;
+    private Button signInButton;
+    private Scene scene;
+    private String cssPath;
     @Override
     public void start(Stage stage) {
         //TODO
