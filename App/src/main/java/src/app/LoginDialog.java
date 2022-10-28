bd60f38add5088c7deb380a8ca2ba7ee25c3b223
ewa.miazga <ewa.miazga@redge.com>
2022-10-28 17:43:26 +0200

    add LoginDialog class

---
+++ App/src/main/java/src/app/LoginDialog.java
@@ -0,0 +1,23 @@
+package src.app;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+
+public class LoginDialog extends Application implements EventHandler<ActionEvent> {
+
+    @Override
+    public void start(Stage primaryStage) {
+        //TODO
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        //TODO
+    }
+}
