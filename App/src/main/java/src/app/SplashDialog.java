e6fd7a404000acd4395cb7c26782371d67ae4945
ewa.miazga <ewa.miazga@redge.com>
2022-12-17 00:26:18 +0100

    Add SplashScreen

---
+++ App/src/main/java/src/app/SplashDialog.java
@@ -0,0 +1,162 @@
+package src.app;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.io.File;
+
+public class SplashDialog extends Application {
+    File file = new File("Logo.png");
+    File file2 = new File("background.png");
+    public final String APPLICATION_ICON =
+            file.toURI().toString();
+    public final String SPLASH_IMAGE =
+            file2.toURI().toString();
+
+    private Pane splashLayout;
+    private ProgressBar loadProgress;
+    private Label progressText;
+
+    private Scene scene;
+    private Stage stage;
+
+    private String cssPath;
+    private static final int SPLASH_WIDTH = 676;
+    private static final int SPLASH_HEIGHT = 227;
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void init() {
+        ImageView splash = new ImageView(new Image(
+                SPLASH_IMAGE
+        ));
+        loadProgress = new ProgressBar();
+        loadProgress.setPrefWidth(SPLASH_WIDTH - 20);
+        progressText = new Label("Will find ports . . .");
+        splashLayout = new VBox();
+        splashLayout.getChildren().addAll(splash, loadProgress, progressText);
+        progressText.setAlignment(Pos.CENTER);
+
+
+        splashLayout.setStyle(
+                "-fx-padding: 5; " +
+                        "-fx-background-color: cornsilk; " +
+                        "-fx-border-width:5; " +
+                        "-fx-border-color: " +
+                        "linear-gradient(" +
+                        "to bottom, " +
+                        "chocolate, " +
+                        "derive(chocolate, 50%)" +
+                        ");"
+        );
+        splashLayout.setEffect(new DropShadow());
+    }
+
+    @Override
+    public void start(final Stage initStage) throws Exception {
+        final Task<ObservableList<String>> friendTask = new Task<ObservableList<String>>() {
+            @Override
+            protected ObservableList<String> call() throws InterruptedException {
+                ObservableList<String> foundFriends =
+                        FXCollections.<String>observableArrayList();
+                ObservableList<String> availableFriends =
+                        FXCollections.observableArrayList(
+                                "Fili", "Kili", "Oin", "Gloin", "Thorin",
+                                "Dwalin", "Balin", "Bifur", "Bofur",
+                                "Bombur", "Dori", "Nori", "Ori"
+                        );
+
+                updateMessage("Finding ports . . .");
+                for (int i = 0; i < availableFriends.size(); i++) {
+                    Thread.sleep(400);
+                    updateProgress(i + 1, availableFriends.size());
+                    String nextFriend = availableFriends.get(i);
+                    foundFriends.add(nextFriend);
+                    updateMessage("Finding ports . . . found " + nextFriend);
+                }
+                Thread.sleep(400);
+                updateMessage("All ports found.");
+
+                return foundFriends;
+            }
+        };
+
+        showSplash(
+                initStage,
+                friendTask,
+                () -> showMainStage()
+        );
+        new Thread(friendTask).start();
+    }
+
+    private void showMainStage() {
+        stage = new Stage(StageStyle.DECORATED);
+        //stage.setTitle("My Ports");
+
+        WelcomeDialog welcomeDialog = new WelcomeDialog();
+        welcomeDialog.start(stage);
+
+
+    }
+
+    private void showSplash(
+            final Stage initStage,
+            Task<?> task,
+            InitCompletionHandler initCompletionHandler
+    ) {
+        progressText.textProperty().bind(task.messageProperty());
+        loadProgress.progressProperty().bind(task.progressProperty());
+        task.stateProperty().addListener((observableValue, oldState, newState) -> {
+            if (newState == Worker.State.SUCCEEDED) {
+                loadProgress.progressProperty().unbind();
+                loadProgress.setProgress(1);
+                initStage.toFront();
+                FadeTransition fadeSplash = new FadeTransition(Duration.seconds(1.2), splashLayout);
+                fadeSplash.setFromValue(1.0);
+                fadeSplash.setToValue(0.0);
+                fadeSplash.setOnFinished(actionEvent -> initStage.hide());
+                fadeSplash.play();
+
+                initCompletionHandler.complete();
+            } // todo add code to gracefully handle other task states.
+        });
+
+        Scene splashScene = new Scene(splashLayout, Color.TRANSPARENT);
+        final Rectangle2D bounds = Screen.getPrimary().getBounds();
+        initStage.setScene(splashScene);
+        initStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - SPLASH_WIDTH / 2);
+        initStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - SPLASH_HEIGHT / 2);
+        initStage.initStyle(StageStyle.TRANSPARENT);
+        initStage.setAlwaysOnTop(true);
+        initStage.show();
+    }
+
+    public interface InitCompletionHandler {
+        void complete();
+    }
+}
+
