8ab797dd7b2b41285372f34f6ba43f6025cfbbfc
mtaudul <mikit2002@wp.pl>
2022-12-26 19:28:59 +0100

    Implemented tests environment

---
+++ App/src/test/java/src/logic/CaptainsEntityTest.java
@@ -0,0 +1,80 @@
+package src.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaptainsEntityTest {
+
+
+    CaptainsEntity myCaptain = new CaptainsEntity("Jan", "Długosz", "10101001", 1);
+    CaptainsEntity myCaptain2 = new CaptainsEntity("Maciek", "Pudzianowski", "11111111111", 13);
+    CaptainsEntity emptyCaptain = new CaptainsEntity("", "", "", 0);
+    @Test
+    void getForename() {
+        assertEquals("Jan", myCaptain.getForename());
+        assertEquals("", emptyCaptain.getForename());
+    }
+
+
+    @Test
+    void setForename() {
+        myCaptain.setForename("Mariusz");
+        assertEquals("Mariusz", myCaptain.getForename());
+    }
+
+    @Test
+    void getSurname() {
+        assertEquals("Długosz", myCaptain.getSurname());
+        assertEquals("", emptyCaptain.getSurname());
+    }
+
+    @Test
+    void setSurname() {
+        myCaptain.setSurname("Kowalski");
+        assertEquals("Kowalski", myCaptain.getSurname());
+    }
+
+    @Test
+    void getPesel() {
+        assertEquals("10101001", myCaptain.getPesel());
+        assertEquals("", emptyCaptain.getPesel());
+    }
+
+    @Test
+    void setPesel() {
+        myCaptain.setPesel("10101010101");
+        assertEquals("10101010101", myCaptain.getPesel());
+    }
+
+    @Test
+    void getCaptainId() {
+        assertEquals(1, myCaptain.getCaptainId());
+        assertEquals(0, emptyCaptain.getCaptainId());
+    }
+
+    @Test
+    void setCaptainId() {
+        myCaptain.setCaptainId(2);
+        assertEquals(2, myCaptain.getCaptainId());
+    }
+
+    @Test
+    void setNegativeCaptainID() {
+        myCaptain.setCaptainId(-1);
+        assertEquals(-1, myCaptain.getCaptainId());
+//        assertThrows(IllegalArgumentException.class, () -> { myCaptain.setCaptainId(-1);});
+    }
+
+    @Test
+    void testEquals() {
+        assertEquals(true, myCaptain2.equals(myCaptain2));
+        var newCaptain2 = new CaptainsEntity("Maciek", "Pudzianowski", "11111111111", 13);
+        assertEquals(true, myCaptain2.equals(newCaptain2));
+        assertEquals(false, myCaptain.equals(myCaptain2));
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
---
+++ App/src/test/java/src/logic/ShipsEntityTest.java
@@ -0,0 +1,64 @@
+package src.logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShipsEntityTest {
+
+    @Test
+    void getCallSign() {
+    }
+
+    @Test
+    void setCallSign() {
+    }
+
+    @Test
+    void getShpipName() {
+    }
+
+    @Test
+    void setShpipName() {
+    }
+
+    @Test
+    void getShipOwnerId() {
+    }
+
+    @Test
+    void setShipOwnerId() {
+    }
+
+    @Test
+    void getShipType() {
+    }
+
+    @Test
+    void setShipType() {
+    }
+
+    @Test
+    void getShipLength() {
+    }
+
+    @Test
+    void setShipLength() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void getShipOwnersByShipOwnerId() {
+    }
+
+    @Test
+    void setShipOwnersByShipOwnerId() {
+    }
+}
