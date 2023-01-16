b6d1a013c41f75c832d8514422030797bbea74ee
ewa.miazga <ewa.miazga@redge.com>
2023-01-16 15:12:16 +0100

    Change file module-info.java

--- App/src/main/java/module-info.java
+++ App/src/main/java/module-info.java
@@ -13,12 +13,12 @@ module src {
     requires java.naming;
     opens src.logic to org.hibernate.orm.core;
 //    requires org.hibernate.commons.annotations;
-    requires transitive org.apiguardian.api;
-    requires transitive org.junit.platform.commons;
-    requires transitive org.opentest4j;
-    requires transitive org.junit.jupiter.api;
-    requires net.bytebuddy;
-    requires org.junit.jupiter.params;
+    //requires transitive org.apiguardian.api;
+    //requires transitive org.junit.platform.commons;
+    //requires transitive org.opentest4j;
+    //requires transitive org.junit.jupiter.api;
+    //requires net.bytebuddy;
+    //requires org.junit.jupiter.params;
     requires java.xml.crypto;
     requires java.desktop;
 }
